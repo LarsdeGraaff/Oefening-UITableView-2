6858cd36feee95eb094d4631491aaf5765c2617d
LarsdeGraaff <lars.dg@gmail.com>
2015-08-25 10:11:45 +0200

    fonts en colors works

---
+++ Oefening-UITableView-2/Oefening-UITableView-2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oefening-UITableView-2
+//
+//  Created by Jean Smits on 24/08/15.
+//  Copyright (c) 2015 Jean Smits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Oefening-UITableView-2/Oefening-UITableView-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oefening-UITableView-2
+//
+//  Created by Jean Smits on 24/08/15.
+//  Copyright (c) 2015 Jean Smits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
